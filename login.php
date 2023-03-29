163c2107e55ff54c3cd3c6625bc7c233854d55d2
eiyahrock <123053962+eiyahrock@users.noreply.github.com>
2023-03-29 16:07:06 +0800

    Initial commit

---
+++ login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body class="container-fluid p-3" >
+<div class="row">
+  <div class="col-3 "></div>
+  <div class="col-6">
+<div class="container p-2" style="border:5px solid black; background-color:pink; border-radius: 20px;">
+<nav class=" p-2 navbar navbar-expand-sm navbar-dark bg-dark" style=" border-radius: 15px;">
+  <a class="bi bi-briefcase-fill navbar-brand" href="login.php"><b> VisVis Travel</b></a>
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="login.php">Login</a>
+      <a class="nav-item nav-link" href="Contact.php">Contact</a>
+      <a class="nav-item nav-link " href="About.php">About</a>
+    </div>
+</nav>
+<div class="row " style="height: 25px;" >
+  <div class="col-4"></div>
+    <label  class="col-7 col-form-label " ><h2 ><b> Welcome Back</b></h2></label>
+</div>
+<div class="row ">
+    <label  class="col-12 col-form-label"><h4><b>Please Enter Your:</b></h4></label>
+</div>
+  <form>
+    <div class="p-1 row" >
+      <label  class="col-sm-2 col-form-label">Email:</label>
+      <div class="col-sm-7" >
+        <input style="background-color:#D8BFD8" type="email" class="form-control" id="inputEmail3" placeholder="Email">
+      </div>
+    </div>
+    <div class="p-1 row">
+      <label  class="col-sm-2 col-form-label">Password:</label>
+      <div class="col-sm-7">
+        <input style="background-color:#D8BFD8" type="password" class="form-control" id="inputPassword3" placeholder="Password">
+      </div>
+    </div>
+    <div class="form-check">
+    <input type="checkbox" class="col-form-label" id="exampleCheck1">
+    <label class="col-sm-3 col-form-label" for="exampleCheck1">Keep me signed in</label>
+    </div>
+  
+    <div class="row">
+      <div class="offset-sm-2 col-sm-10">
+        <button type="submit" class="btn btn-dark" >Sign in</button>
+      </div>
+    </div>
+    <!-- <img src="https://images.unsplash.com/photo-1602242896752-b5c57d3f395f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80" class="col-sm-3" alt="Cinque Terre"> -->
+  </form>
+</div>
+  </div>
+</div>
+<div class="col-sm"></div>
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</html>
